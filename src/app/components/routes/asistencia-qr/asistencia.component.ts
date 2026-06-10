c75ffb60981b1e83afbca20b16c4b6015059c4d5
Facundo Erbin <facuerbin@gmail.com>
2026-06-10 01:22:32 -0300

    Fix change detection in QR attendance view

--- src/app/components/routes/asistencia-qr/asistencia.component.ts
+++ src/app/components/routes/asistencia-qr/asistencia.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { AuthService } from 'src/app/services/auth/auth.service';
 @Component({
   standalone: false,
@@ -10,13 +10,14 @@ export class AsistenciaQRComponent implements OnInit {
     orgName: string = '';
     url: string = '';
 
-    constructor(private auth: AuthService) {
+    constructor(private auth: AuthService, private cdr: ChangeDetectorRef) {
     }
 
     async ngOnInit(): Promise<void> {
       const org = await this.auth.getOrganizationDetail();
       this.url = this.auth.getAsistenciaUrl() + org.id;
       this.orgName = org.nombre;
+      this.cdr.detectChanges();
     }
 
     print() {
